3f6325b6a85a39b05bc6930d61643699ff90058c
heeseon0429 <112688192+heeseon0429@users.noreply.github.com>
2022-10-27 18:55:08 +0900

    Add files via upload
    22.10.27

--- 5_plugin/4_form/js/scripts.js
+++ 5_plugin/4_form/js/scripts.js
@@ -1,3 +1,38 @@
 /**
  * 
  */
+ $(function(){
+	$('#signup > form').validate({
+		rules : {
+			name  : {required: true},
+			email : {required: true,
+						email : true //@ 같은 것들은 걸러줌
+			},
+			website : {url:true},
+			password : {
+						minlength : 6,
+						maxlength : 12,
+			},//password
+			passconf : {equalTo : '#password'}
+		},//rules
+		success : function(label){  //
+			label.addClass('valid');
+			label.text('성공');
+		}
+	});
+	
+	//------------------------------------------------
+	// attr() -> prop()
+	$('.check-all').click(function(){			
+		if($('.check-all').is(':checked')){	//check-all 클릭했을 때
+			$("input[name=agree]").prop('checked',true);//check(O) : true
+		}else{
+			$("input[name=agree]").prop('checked',false);//check(X) : false
+		}
+	});//check 항목
+	
+	$('input.check-all').click(function(){ 
+      $('input.agree').prop('checked', $('input.check-all').is(':checked')) 
+   });
+	
+})//function
---
+++ 5_plugin/7_bxSlider/script.js
@@ -0,0 +1,8 @@
+$(function(){
+	  $('#banner_wrap').bxSlider({
+	    mode: 'fade',
+	    captions: true,
+	    slideWidth: 600
+	  });
+	});
+
